2e24bd608351925351011f2649a009cb62b14044
KimBoWoon <haioer@naver.com>
2016-04-30 21:15:01 +0900

    modify

--- calculator/app/src/main/java/com/example/secret/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/secret/calculator/MainActivity.java
@@ -2,15 +2,10 @@ package com.example.secret.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.beardedhen.androidbootstrap.TypefaceProvider;
 
 
 public class MainActivity extends Activity {
